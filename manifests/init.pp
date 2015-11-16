d5863a70a314006128fd0735203f0e69ec23e10c
hzhu16 <nautilushj@gmail.com>
2015-11-16 14:01:26 -0600

    init.pp updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,53 +12,53 @@ class osgwnclient {
 		require => Package["yum-priorities"],
 	}
 
-	package { glexec:
-		name => "glexec.x86_64",
-		ensure => installed,
-		require => Package["yum-priorities"],
-	}
+	#package { glexec:
+	#	name => "glexec.x86_64",
+	#	ensure => installed,
+	#	require => Package["yum-priorities"],
+	#}
 
-	package { lcmaps:
-		name => "lcmaps.x86_64",
-		ensure => latest,
-		require => Package["yum-priorities"],
-	}
+	#package { lcmaps:
+	#	name => "lcmaps.x86_64",
+	#	ensure => latest,
+	#	require => Package["yum-priorities"],
+	#}
 
-	package { "lcmaps-plugins-process-tracking.x86_64":
-		ensure => latest,
-		require => Package["yum-priorities"],
-	}
+	#package { "lcmaps-plugins-process-tracking.x86_64":
+	#	ensure => latest,
+	#	require => Package["yum-priorities"],
+	#}
 
-	package { "lcmaps-plugins-mount-under-scratch.x86_64":
-		ensure => latest,
-		require => Package["yum-priorities"],
-	}
-        package { "lcmaps-plugins-scas-client.x86_64":
-		ensure => latest,
-		require => Package["yum-priorities"],
-	}
+	#package { "lcmaps-plugins-mount-under-scratch.x86_64":
+	#	ensure => latest,
+	#	require => Package["yum-priorities"],
+	#}
+  #      package { "lcmaps-plugins-scas-client.x86_64":
+	#	ensure => latest,
+	#	require => Package["yum-priorities"],
+	#}
 
 	package { "symlinks":
 		ensure => installed,
 		require => Package["yum-priorities"],
 	}
 
-	file { "glexec.conf":
-		path    => "/etc/glexec.conf",
-		owner   => "root", group => "glexec", mode => 640,
-		ensure  => present,
-		require => Package["glexec"],
-		content => template("osg-wn-client/glexec.conf.erb"),
-	}
+	#file { "glexec.conf":
+	#	path    => "/etc/glexec.conf",
+	#	owner   => "root", group => "glexec", mode => 640,
+	#	ensure  => present,
+	#	require => Package["glexec"],
+	#	content => template("osg-wn-client/glexec.conf.erb"),
+	#}
 
-	file { "lcmaps.db":
-		path    => "/etc/lcmaps.db",
-		owner   => "root", group => "root", mode => 644,
-		require => [Package["lcmaps"],
-			    Package["lcmaps-plugins-mount-under-scratch.x86_64"],
-			    Package["lcmaps-plugins-process-tracking.x86_64"]],
-		content => template("osg-wn-client/lcmaps.db.erb"),
-	}
+	#file { "lcmaps.db":
+	#	path    => "/etc/lcmaps.db",
+	#	owner   => "root", group => "root", mode => 644,
+	#	require => [Package["lcmaps"],
+	#		    Package["lcmaps-plugins-mount-under-scratch.x86_64"],
+	#		    Package["lcmaps-plugins-process-tracking.x86_64"]],
+	#	content => template("osg-wn-client/lcmaps.db.erb"),
+	#}
 
 	## Torque *requires* a home directory to be present.  Sigh.
 	file { "/grid_home":
@@ -74,5 +74,14 @@ class osgwnclient {
 		ensure	=> "stopped",
         }
 
+	mount { "/var/lib/condor-ce":
+		name	=> "/var/lib/condor-ce",
+		ensure	=> mounted,
+		device	=> "ce1.sandhills.hcc.unl.edu:/var/lib/condor-ce",
+		fstype	=> "nfs",
+		options	=> "defaults,soft",
+		remounts	=> true,
+		target	=> "/etc/fstab",
+		}
 
 }
