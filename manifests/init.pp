4d0fd26f3add45fc517a699ac78276adbac659f7
John Thiltges <jthiltges2@unl.edu>
2017-07-11 11:28:49 -0500

    Remove unused references to glexec and lcmaps

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,53 +17,10 @@ class osgwnclient {
     ensure => latest,
   }
 
-  #package { glexec:
-  #  name => 'glexec.x86_64',
-  #  ensure => installed,
-  #  require => Package['yum-priorities'],
-  #}
-
-  #package { lcmaps:
-  #  name => 'lcmaps.x86_64',
-  #  ensure => latest,
-  #  require => Package['yum-priorities'],
-  #}
-
-  #package { 'lcmaps-plugins-process-tracking.x86_64':
-  #  ensure => latest,
-  #  require => Package['yum-priorities'],
-  #}
-
-  #package { 'lcmaps-plugins-mount-under-scratch.x86_64':
-  #  ensure => latest,
-  #  require => Package['yum-priorities'],
-  #}
-  #      package { 'lcmaps-plugins-scas-client.x86_64':
-  #  ensure => latest,
-  #  require => Package['yum-priorities'],
-  #}
-
   package { 'symlinks':
     ensure  => installed,
     require => Package['yum-priorities'],
   }
-
-  #file { 'glexec.conf':
-  #  path    => '/etc/glexec.conf',
-  #  owner   => 'root', group => 'glexec', mode => 640,
-  #  ensure  => present,
-  #  require => Package['glexec'],
-  #  content => template('osg-wn-client/glexec.conf.erb'),
-  #}
-
-  #file { 'lcmaps.db':
-  #  path    => '/etc/lcmaps.db',
-  #  owner   => 'root', group => 'root', mode => 644,
-  #  require => [Package['lcmaps'],
-  #        Package['lcmaps-plugins-mount-under-scratch.x86_64'],
-  #        Package['lcmaps-plugins-process-tracking.x86_64']],
-  #  content => template('osg-wn-client/lcmaps.db.erb'),
-  #}
 
   ## Torque *requires* a home directory to be present.  Sigh.
   file { '/grid_home':
